777dfe2c6b80bfba95ad934cd6811e11be907018
william <w.crollet@gmail.com>
2024-10-08 17:01:47 +0200

    start

--- setup.py
+++ setup.py
@@ -1,1 +1,31 @@
+import pygame
+import sys
 
+
+#een object game
+class Game:
+    def __init__(self):
+        
+        pygame.init()
+
+        pygame.display.set_caption("tactics & Conquer")
+        self.screen = pygame.display.set_mode((640,480))
+
+
+        self.clock =pygame.time.Clock()
+
+    def run(self):
+    # de run functie runt de game
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            
+            pygame.display.update()
+            self.clock.tick(60)
+
+
+
+
+Game().run()
