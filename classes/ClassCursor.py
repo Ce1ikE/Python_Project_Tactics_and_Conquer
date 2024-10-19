2e3cb951176da07280b029c09d5226072f2f800d
crollie-1 <w.crollet@gmail.com>
2024-10-19 10:49:01 +0200

    Revert "cursor class progess"
    This reverts commit cdcebf6a8f334c7887118d1d2bb98314a1e90dbb.

--- classes/ClassCursor.py
+++ classes/ClassCursor.py
@@ -3,39 +3,31 @@ import pygame_gui
 from data.Configuration import *
 
 
-
-
-
 class Cursor:
-    def __init__(self,x,y):
-        self.X = x
-        self.Y = y
-        self.cursor_IMG = pygame.image.load("./assets/png/cursor.png")
 
-    def showCursor(self,surface):
+    def __init__(self,pos: list) -> None:
+        self.position = pos
+
+
+
+
+
+    def showCursor():
         print("showed cursor")
-        print((self.X * 16, self.Y * 16))
-        surface.blit(self.cursor_IMG, (self.X * TILESIZE, self.Y * TILESIZE))
 
     def hideCursor():
         print("hide cursor")
-        #moeten de cursor overschrijven met iets
 
     
-    def UpdatePosition(self,direction):
+    def UpdatePosition(direction):
         if(direction =="UP"):
-            self.Y -= 1
+            print("cursor moves up")
         elif(direction =="DOWN"):
             print("cursor moves down")
-            self.Y += 1
-            
         elif(direction == "LEFT"):
             print("cursor moves left")
-            self.X -= 1
-
         elif(direction == "RIGHT"):
             print("cursor moves right")
-            self.X += 1
         else:
             print("cursor did not get the right argument")
-#still needs to check wheter the cursor is out of bounds 
+
--- setup.py
+++ setup.py
@@ -4,10 +4,9 @@ import pygame_gui
 import sys
 from classes.ClassGrid import Grid
 from classes.ClassMenu import Menu
-from classes.ClassGame import Game
-from pytmx.util_pygame import load_pygame
-from data.Configuration import *
-Game().createMap()
 
-# test = unit(INFANTRY,[0,0])
-# print(test)
+from data.Configuration import *
+#Game().createMap()
+
+test = unit(INFANTRY,[0,0])
+print(test)
