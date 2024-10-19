025deee39b94b929c02928a991e6031540e91ca0
crollie-1 <w.crollet@gmail.com>
2024-10-19 16:58:23 +0200

    Revert "unit class methodes"
    This reverts commit 606b38887f61bf4dce8dc22a044667384ad6ff3c.

--- setup.py
+++ setup.py
@@ -8,7 +8,5 @@ from classes.ClassMenu import Menu
 from data.Configuration import *
 #Game().createMap()
 
-team1 = unit(INFANTRY,0,0,1)
-team2 = unit(INFANTRY,1,0,2)
-
-team1.attack(team2)
+test = unit(INFANTRY,0, 0)
+print(test)
